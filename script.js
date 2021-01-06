436b0a1da320f562d3caebe2cc4a3f4b915775ab
Sean-Drew <SDrew989@gmail.com>
2021-01-06 13:17:49 -0500

    Add game starting functions
    Added functions startGame and playGame to set up game-starting logic.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+// Global variables
 const btnStart = document.querySelector('.btnStart')
+const gameOverEle = document.getElementById('gameOverEle')
+let gamePlay = false
+let player
+let animateGame
+
+// Event listeners
 btnStart.addEventListener('click', startGame)
 
 function startGame () {
-    console.log('clicked')
+    gamePlay = true
+    gameOverEle.style.display = 'none'
+    player = {
+        score: 0,
+        barWidth: 100,
+        lives: 100,
+    }
+    animateGame = requestAnimationFrame(playGame)
+}
+
+function playGame () {
+    console.log('game in play')
+    animateGame = requestAnimationFrame(playGame)
 }
