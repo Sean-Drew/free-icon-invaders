6f06c362994334a6e17a716aad21109ac45833b7
Sean-Drew <SDrew989@gmail.com>
2021-01-06 14:56:14 -0500

    Add some mouse movement logic
    Added logic to identify mouse position relative to the center of the
    game turret which will eventually move the turret to face the mouse
    direction.

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 // Global variables
 const btnStart = document.querySelector('.btnStart')
 const gameOverEle = document.getElementById('gameOverEle')
+const container = document.getElementById('container')
+const box = document.querySelector('.box')
+const boxCenter = [ 
+    box.offsetLeft + (box.offsetWidth / 2),
+    box.offsetTop + (box.offsetHeight / 2)
+]
+// console.log('boxCenter is: ', boxCenter)
+
 let gamePlay = false
 let player
 let animateGame
 
 // Event listeners
 btnStart.addEventListener('click', startGame)
+container.addEventListener('mousedown', mouseDown)
+container.addEventListener('mousemove', movePosition)
+
+function movePosition (e) {
+    // console.log('event is: ', e)
+    let mouseAngle = getDeg(e)
+    console.log('mouseAngle is: ', mouseAngle)
+}
+
+function getDeg (e) {
+    let angle = Math.atan2(e.clientX - boxCenter[0], - (e.clientY - boxCenter[1]))
+    return angle * (180 / Math.PI)
+}
+
+function mouseDown (e) {
+    if (gamePlay) {
+        console.log('from mousedown, FIRE')
+    }
+}
 
 function startGame () {
     gamePlay = true
@@ -16,10 +43,16 @@ function startGame () {
         barWidth: 100,
         lives: 100,
     }
+    // setup bad guys
     animateGame = requestAnimationFrame(playGame)
 }
 
 function playGame () {
-    console.log('game in play')
-    animateGame = requestAnimationFrame(playGame)
+    if (gamePlay) {
+        // console.log('game in play')
+        // move shots
+        // update dashboard
+        // move enemy
+        animateGame = requestAnimationFrame(playGame)
+    }
 }
